558a5a19db9734fc06b7deb293dd1f8fdebbef79
student <student@virtualbox>
2016-10-12 11:58:30 +0200

    some test refactor

--- src/test/java/domain/MoneyTest.java
+++ src/test/java/domain/MoneyTest.java
@@ -9,16 +9,14 @@ public class MoneyTest {
 	@Test
 	public void testMultiplication(){
 		Dollar five = new Dollar(5);
-		Dollar product = five.times(2);
-		assertEquals(10, product.amount);
-		product = five.times(3);
-		assertEquals(15, product.amount);
+		assertEquals(new Dollar(10), five.times(2));
+		assertEquals(new Dollar(15), five.times(3));
 	}
 	
 	@Test
 	public void testEquality(){
 		assertEquals(new Dollar(5), new Dollar(5));
-		assertNotEquals(new Dollar(5), new Dollar(10));
+		assertNotEquals(new Dollar(5), new Dollar(15));
 	}
 
 }
