a08497ad442a7a631789487890b0ddd29f13d175
Guziec-Lobuziec <oliversliwonik@o2.pl>
2016-10-26 12:19:05 +0200

    different currency summing

--- src/main/java/domain/Sum.java
+++ src/main/java/domain/Sum.java
@@ -11,7 +11,8 @@ public class Sum implements Expression {
 	}
 
 	public Money reduce(Bank bank, String currency) {
-		return new Money(addend.amount + augend.amount,currency);
+		return new Money(addend.reduce(bank, currency).amount
+				+ augend.reduce(bank, currency).amount,currency);
 	}
 
 }
