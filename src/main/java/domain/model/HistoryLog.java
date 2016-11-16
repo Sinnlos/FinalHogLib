5f9782a9c1d6b2a3409f7f7a14abf84943fc40d2
student <student@virtualbox>
2016-11-16 13:07:53 +0100

    poprawki drobne

--- src/main/java/domain/model/HistoryLog.java
+++ src/main/java/domain/model/HistoryLog.java
@@ -13,20 +13,12 @@ public class HistoryLog {
 	private Account to;
 	private double rate;
 	private Operation type;
-	private int id;
-	
 	
 	public Account getAccount() {
 		return account;
 	}
 	public void setAccount(Account account) {
 		this.account = account;
-	}
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
 	}
 	public Operation getType() {
 		return type;
