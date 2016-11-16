332236e9453b555a65f0077913b1d18c906d393b
student <student@virtualbox>
2016-11-16 10:30:48 +0100

    added getters and setters

--- src/main/java/domain/model/Person.java
+++ src/main/java/domain/model/Person.java
@@ -7,5 +7,24 @@ public class Person {
 	private String name;
 	private String surname;
 	private List<Account> accounts;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	public List<Account> getAccounts() {
+		return accounts;
+	}
+	public void setAccounts(List<Account> accounts) {
+		this.accounts = accounts;
+	}
+	
 }
 
