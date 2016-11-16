f72b3cae28132ca74e9331e7e9bc36348a35d0d1
student <student@virtualbox>
2016-11-16 11:07:35 +0100

    merge done

--- src/main/java/domain/App.java
+++ src/main/java/domain/App.java
@@ -1,13 +1,26 @@
 package domain;
 
-/**
- * Hello world!
- *
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import dao.PersonRepository;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	String url = "jdbc:hsqldb:hsql://localhost/workdb";
+    	try {
+			Connection connection = DriverManager.getConnection(url);
+			
+			PersonRepository repo = new PersonRepository(connection);
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+    	
+        System.out.println( "Koniec" );
+        
     }
 }
---
+++ src/main/java/domain/model/EnumDictionary.java
@@ -0,0 +1,43 @@
+package domain.model;
+
+public class EnumDictionary {
+
+	private int id;
+	private int intKey;
+	private String stringKey;
+	private String value;
+	private String enumName;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getIntKey() {
+		return intKey;
+	}
+	public void setIntKey(int intKey) {
+		this.intKey = intKey;
+	}
+	public String getStringKey() {
+		return stringKey;
+	}
+	public void setStringKey(String stringKey) {
+		this.stringKey = stringKey;
+	}
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
+	public String getEnumName() {
+		return enumName;
+	}
+	public void setEnumName(String enumName) {
+		this.enumName = enumName;
+	}
+	
+	
+}
